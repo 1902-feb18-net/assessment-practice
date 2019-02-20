fe78c166d087e8f8caa7ef98668ddc00b6922d8c
Guillermo <randomfood1@gmail.com>
2019-02-20 10:37:03 -0600

    Added gitignore and fizzbuzz project

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,43 @@
+using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int x = 1;
+           int fizz = 0;
+           int buzz = 0;
+           int fizzbuzz = 0;
+
+           while (x != 1001)
+           {
+               if(x%3 == 0 && x%5 != 0)
+               {
+                   Console.WriteLine("Fizz ");
+                   fizz++;
+               }
+               else if(x%5 == 0 && x%3 != 0)
+               {
+                   Console.WriteLine("Buzz ");
+                   buzz++;
+               }
+               else if(x%3 == 0 && x%5 == 0)
+               {
+                   Console.WriteLine("FizzBuzz ");
+                   fizzbuzz++;
+               }
+               else 
+               {
+                   Console.WriteLine($" {x} ");
+               }
+               x++;
+           }
+
+           Console.WriteLine($"Total Number of Fizz: {fizz}");
+           Console.WriteLine($"Total Number of Buzz: {buzz}");
+           Console.WriteLine($"Total Number of FizzBuzz: {fizzbuzz}");
+        }
+    }
+}
