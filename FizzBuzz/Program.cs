df4bd838141c4927585d1ace243dd72d0bba45d1
William <billycraig2009@hotmail.com>
2019-02-20 10:33:01 -0600

    new fizzbuzz counter program

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fizz = 0;
+            int buzz = 0;
+            int fizzbuzz = 0;
+            for (int ii = 1; ii <= 1000; ii++)
+            {
+                if( ii % 3 == 0 && ii % 5 == 0)
+                {
+                    fizzbuzz++;
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if( ii % 3 == 0 && ii % 5 != 0)
+                {
+                    fizz++;
+                    Console.WriteLine("Fizz");
+                } 
+                else if (ii % 3 != 0 && ii % 5 == 0)
+                {
+                    buzz++;
+                    Console.WriteLine("Buzz");
+                } 
+                else
+                {
+                    Console.WriteLine(ii);    
+                }
+            }
+
+            Console.WriteLine($"Totals: Fizz: {fizz} Buzz: {buzz} FizzBuzz: {fizzbuzz}");
+        }
+    }
+}
