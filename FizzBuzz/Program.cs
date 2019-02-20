8e5b7d5841349dc68b5d6bf4164b5c5d63b9b171
Presley Dyer <presleyhdyer@gmail.com>
2019-02-20 10:42:24 -0600

    added total counts for fizz, buzz, and fizzbuzz :wq

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -6,6 +6,9 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
+            int CountFizz = 0;
+            int CountBuzz = 0;
+            int CountFizzBuzz = 0;
             for(int i = 1; i <= 1000; i++)
             {
                 if(i % 3 == 0)
@@ -13,21 +16,27 @@ namespace FizzBuzz
                     if(i % 5 == 0)
                     {
                         Console.WriteLine($"{i} FizzBuzz");
+                        CountFizzBuzz++;
                     } else
                     {
                         Console.WriteLine($"{i} Fizz");
+                        CountFizz++;
                     }
                 } else
                 {
                     if(i % 5 == 0)
                     {
                         Console.WriteLine($"{i} Buzz");
+                        CountBuzz++;
                     } else
                     {
                         Console.WriteLine($"{i}");
                     }
                 }
             }
+            Console.WriteLine($"Total Fizz: {CountFizz}");
+            Console.WriteLine($"Total Buzz: {CountBuzz}");
+            Console.WriteLine($"Total FizzBuzz: {CountFizzBuzz}");
         }
     }
 }
