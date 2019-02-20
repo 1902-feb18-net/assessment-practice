bc9a0df4bb864d450bac2533119392b664b1aa5e
Diana Juarez <dianajuarez@Dianas-MacBook-Pro.local>
2019-02-20 10:45:26 -0600

    added in the assesment practice code for fizzbuzz

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+
+            int fizz = 0;
+            int buzz = 0;
+            int fizzbuzz = 0;
+            for (int i = 1; i <= 1000; i++)
+            {
+                if (((i % 3) == 0) && ((i % 5) == 0))
+                {
+                    Console.WriteLine("Fizz Buzz ");
+                    fizzbuzz++;
+                }
+                else if ((i % 3) == 0)
+                {
+                    Console.WriteLine("Fizz ");
+                    fizz++;
+                }
+                else if ((i % 5) == 0)
+                {
+                    Console.WriteLine("Buzz ");
+                    buzz++;
+                }
+                else
+                {
+                    Console.WriteLine($"{i} ");
+                }
+            }
+
+            Console.WriteLine($"Fizz: {fizz}");
+            Console.WriteLine($"Buzz: {buzz}");
+            Console.WriteLine($"FizzBuzz: {fizzbuzz}");
+
+        }
+
+    }
+}
