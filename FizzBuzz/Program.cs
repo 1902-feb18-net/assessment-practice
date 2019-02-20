860ac6762bb2a4bd98e82cc3e9bf049f14e8a12a
Daniel Rendon <danielrendon93@gmail.com>
2019-02-20 10:44:12 -0600

    count FizzBuzz implementation

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -6,7 +6,35 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int countFizz = 0;
+            int countBuzz = 0;
+            int countFizzBuzz = 0;
+
+            for (int i = 1; i < 1001; i++)
+            {
+                if ((i % 3 == 0) && (i % 5 != 0))
+                {
+                    Console.WriteLine("Fizz");
+                    countFizz++;
+                }
+                else if ((i % 5 == 0) && (i % 3 != 0))
+                {
+                    Console.WriteLine("Buzz");
+                    countBuzz++;
+                }
+                else if ((i % 5 == 0) && (i % 3 == 0))
+                {
+                    Console.WriteLine("FizzBuzz");
+                    countFizzBuzz++;
+                }
+                else if ((i % 5 != 0) && (i % 3 != 0))
+                {
+                    Console.WriteLine(i);        
+                }
+            }
+            Console.WriteLine("Fizz: {0}", countFizz);
+            Console.WriteLine("Buzz: {0}", countBuzz);
+            Console.WriteLine("FizzBuzz: {0}", countFizzBuzz);
         }
     }
 }
