63e4be8f98542cdaeaf80d26e13f10530c6ae218
Conrad <conrad.trohajr@gmail.com>
2019-02-20 11:27:46 -0500

    FizzBuzz program finished!

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -6,7 +6,36 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int fizzCount = 0;
+            int buzzCount = 0;
+            int fizzBuzzCount = 0;
+
+            for(int i=1; i<=1000; i++)
+            {
+                if(i%3 == 0 && i%5 != 0)
+                {
+                    Console.WriteLine("Fizz");
+                    fizzCount++;
+                }
+                else if(i%5 == 0 && i%3 != 0)
+                {
+                    Console.WriteLine("Buzz");
+                    buzzCount++;
+                }
+                else if(i%3 == 0 && i%5 == 0)
+                {
+                    Console.WriteLine("Fizzbuzz");
+                    fizzBuzzCount++;
+                }
+                else{
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine($"Fizz Count:     {fizzCount}");
+            Console.WriteLine($"Buzz Count:     {buzzCount}");
+            Console.WriteLine($"Fizzbuzz Count: {fizzBuzzCount}");
         }
     }
 }
