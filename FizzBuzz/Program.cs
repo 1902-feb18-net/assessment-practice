7a22dfaabbb652b9487039e26acbbacc1a43e815
William <billycraig2009@hotmail.com>
2019-02-20 10:53:16 -0600

    william version fizzbuzz

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fizz = 0;
+            int buzz = 0;
+            int fizzbuzz = 0;
+
+            for(int ii = 1; ii <= 1000; ii++)
+            {
+                if(ii % 3 == 0 && ii % 5 == 0)
+                {
+                    Console.WriteLine("fizzbuzz");
+                    fizzbuzz++;
+                }
+                else if(ii % 3 == 0 && ii % 5 != 0) 
+                {
+                    Console.WriteLine("fizz");
+                    fizz++;
+                }
+                else if (ii % 3 != 0 && ii % 5 == 0)
+                {
+                    Console.WriteLine("buzz");
+                    buzz++;
+                }
+                else
+                {
+                    Console.WriteLine(ii);
+                }
+            }
+
+            Console.WriteLine($"Total counts Fizz: {fizz} Buzz: {buzz} FizzBuzz: {fizzbuzz}");
+        }
+    }
+}
